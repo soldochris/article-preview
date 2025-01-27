d994c9964dddd897a0bcd5b51d1abd8442411e91
Christian Soldevilla <soldevilla.christian@gmail.com>
2025-01-27 13:23:52 -0500

    design ready for mobile

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const shareButton = document.querySelector('.share-button');
+const shareContent = document.querySelector('.share-content');
+
+addEventListener('click', () => { 
+  shareContent.classList.toggle('show');
+});
