3b6ca36942ad1182db6fc10e41ae1f88ba04a187
Christian Soldevilla <soldevilla.christian@gmail.com>
2025-01-27 15:41:13 -0500

    design ready for desktop

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const shareButton = document.querySelector('.share-button');
+const shareButton = document.querySelector('.share-icon');
 const shareContent = document.querySelector('.share-content');
 
-addEventListener('click', () => { 
+shareButton.addEventListener('click', () => {
   shareContent.classList.toggle('show');
-});
+})
